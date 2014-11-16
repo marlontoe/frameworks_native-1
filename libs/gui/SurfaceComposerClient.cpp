b882ef239a3beb6f4676286791fd6eb5263ac10f
mickybart <mickybart@pygoscelis.org>
2014-11-16 15:00:37 -0500

    check getDisplayInfo return
    Change-Id: I6b09d46496025f16e13367891041ae3ad4ec4b94

--- libs/gui/SurfaceComposerClient.cpp
+++ libs/gui/SurfaceComposerClient.cpp
@@ -806,13 +806,15 @@ status_t SurfaceComposerClient::getDisplayInfo(
 
 ssize_t SurfaceComposerClient::getDisplayWidth(int32_t displayId) {
     DisplayInfo info;
-    getDisplayInfo(getBuiltInDisplay(displayId), &info);
+    if (getDisplayInfo(getBuiltInDisplay(displayId), &info) != NO_ERROR)
+        return 0;
     return info.w;
 }
 
 ssize_t SurfaceComposerClient::getDisplayHeight(int32_t displayId) {
     DisplayInfo info;
-    getDisplayInfo(getBuiltInDisplay(displayId), &info);
+    if (getDisplayInfo(getBuiltInDisplay(displayId), &info) != NO_ERROR)
+        return 0;
     return info.h;
 }
 
